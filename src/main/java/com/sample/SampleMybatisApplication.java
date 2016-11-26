e0092032af54563a575bd04911f90e79fdda46a8
zhenghuasheng <zhenghuasheng@aixuedai.com>
2016-11-26 19:44:27 +0800

    jenkins package jar

--- src/main/java/com/sample/SampleMybatisApplication.java
+++ src/main/java/com/sample/SampleMybatisApplication.java
@@ -15,21 +15,11 @@
  */
 
 package com.sample;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.log4j.Logger;
-import org.apache.tomcat.jdbc.pool.DataSource;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 
 
 @SpringBootApplication
@@ -69,6 +59,9 @@ public class SampleMybatisApplication{
 		 * Main Start
 		 */
 		public static void main(String[] args) {
+//			SpringApplication application = new SpringApplication(SampleMybatisApplication.class);
+//			application.setShowBanner(false);
+//			application.run(args);
 			SpringApplication.run(SampleMybatisApplication.class, args);
 			logger.info("============= SpringBoot Start Success =============");
 		}
